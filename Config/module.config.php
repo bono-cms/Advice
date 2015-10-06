da7c1ebe311ecf30e89ba4c404b583852a8e6256
David Yang <daworld.ny@gmail.com>
2015-10-07 03:49:51 +0500

    Moved Advice to standalone repository

---
+++ Config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+return array(
+ 'module' => array(
+  'name'	=> 'Advice',
+  'url'	=> '/admin/module/advice',
+  'icon'	=> '/module/Advice/Assets/advice.png',
+  'order' => 15,
+  'description' => 'Advice module allows you to manage advices on your site'
+  )
+);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Advice
 Advice module
+=============
+
+Advice module can be used to show random advices on your site. For details, please refer its documentation.
