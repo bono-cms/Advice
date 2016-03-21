2c2a3dbbf943d73b2753d05ecc456d9dbe0c8e6d
David Yang <daworld.ny@gmail.com>
2016-03-21 21:06:55 +0500

    Added support for table prefix

--- Storage/MySQL/AdviceMapper.php
+++ Storage/MySQL/AdviceMapper.php
@@ -21,7 +21,7 @@ final class AdviceMapper extends AbstractMapper implements AdviceMapperInterface
      */
     public static function getTableName()
     {
-        return 'bono_module_advice';
+        return self::getWithPrefix('bono_module_advice');
     }
 
     /**
