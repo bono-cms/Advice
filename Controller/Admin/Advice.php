e95287aaadb0174d7e0ac211a89a32116e2378f7
dave-yang <daworld.ny@gmail.com>
2018-12-19 13:05:36 +0500

    Added missing batch removal

--- Controller/Admin/Advice.php
+++ Controller/Admin/Advice.php
@@ -118,8 +118,8 @@ final class Advice extends AbstractController
         $service = $this->getModuleService('adviceManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
