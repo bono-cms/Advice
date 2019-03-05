8f72e3d43d2b9b2bbfbefbbfcc6a85bc5580819c
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:13:32 +0500

    Updated module config

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'caption' => 'Advice',
- 'name' => 'Advice',
- 'route' => 'Advice:Admin:Advice@gridAction',
- 'order' => 15,
- 'description' => 'Advice module allows you to manage advices on your site'
+    'caption' => 'Advice',
+    'description' => 'Advice module allows you to manage advices on your site',
+    'menu' => array(
+        'name' => 'Advice',
+        'icon' => 'fa fa-hashtag fa-5x',
+        'items' => array(
+            array(
+                'route' => 'Advice:Admin:Advice@gridAction',
+                'name' => 'View all advices'
+            ),
+            array(
+                'route' => 'Advice:Admin:Advice@addAction',
+                'name' => 'Add new advice'
+            )
+        )
+    )
 );
