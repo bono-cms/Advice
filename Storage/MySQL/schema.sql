b2655caae29a7db0cb07ee2af30829ac0290ef7e
dave-yang <daworld.ny@gmail.com>
2019-03-16 23:32:48 +0500

    Added missing FK

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE `bono_module_advice` (
     `category_id` INT DEFAULT NULL COMMENT 'Optional category ID',
     `published` varchar(1) NOT NULL COMMENT 'Whether advice is enabled',
     `icon` varchar(255) NOT NULL COMMENT 'Optional icon'
+
 ) DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_advice_translations`;
@@ -12,11 +13,14 @@ CREATE TABLE `bono_module_advice_translations` (
     `id` INT NOT NULL COMMENT 'Advice ID',
     `lang_id` INT NOT NULL COMMENT 'Attached language ID',
     `title` varchar(255) NOT NULL COMMENT 'Advice title',
-    `content` LONGTEXT NOT NULL COMMENT 'Advice description'
+    `content` LONGTEXT NOT NULL COMMENT 'Advice description',
+
+    FOREIGN KEY (id) REFERENCES bono_module_advice(id) ON DELETE CASCADE
+
 ) DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_advice_categories`;
 CREATE TABLE `bono_module_advice_categories` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Category ID',
-    `name` varchar(255) NOT NULL COMMENT 'Category name'
+    `name` varchar(255) NOT NULL COMMENT 'Category name'   
 ) DEFAULT CHARSET = UTF8;
