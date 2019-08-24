01706deb8a850b4088101c296c8fd58526311232
dave-yang <daworld.ny@gmail.com>
2019-08-24 16:25:18 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,11 +1,20 @@
 
+/* Categories */
+DROP TABLE IF EXISTS `bono_module_advice_categories`;
+CREATE TABLE `bono_module_advice_categories` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Category ID',
+    `name` varchar(255) NOT NULL COMMENT 'Category name'   
+) DEFAULT CHARSET = UTF8;
+
+/* Advices */
 DROP TABLE IF EXISTS `bono_module_advice`;
 CREATE TABLE `bono_module_advice` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Advice ID',
     `category_id` INT DEFAULT NULL COMMENT 'Optional category ID',
     `published` varchar(1) NOT NULL COMMENT 'Whether advice is enabled',
-    `icon` varchar(255) NOT NULL COMMENT 'Optional icon'
+    `icon` varchar(255) NOT NULL COMMENT 'Optional icon',
 
+    FOREIGN KEY (category_id) REFERENCES bono_module_advice_categories(id) DELETE SET NULL ON UPDATE CASCADE
 ) DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_advice_translations`;
@@ -15,12 +24,6 @@ CREATE TABLE `bono_module_advice_translations` (
     `title` varchar(255) NOT NULL COMMENT 'Advice title',
     `content` LONGTEXT NOT NULL COMMENT 'Advice description',
 
-    FOREIGN KEY (id) REFERENCES bono_module_advice(id) ON DELETE CASCADE
-
-) DEFAULT CHARSET = UTF8;
-
-DROP TABLE IF EXISTS `bono_module_advice_categories`;
-CREATE TABLE `bono_module_advice_categories` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Category ID',
-    `name` varchar(255) NOT NULL COMMENT 'Category name'   
+    FOREIGN KEY (id) REFERENCES bono_module_advice(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
 ) DEFAULT CHARSET = UTF8;
