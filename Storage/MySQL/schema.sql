ba55ba98a2a4a33852701810a97380717f16060f
dave-yang <daworld.ny@gmail.com>
2019-08-24 16:29:31 +0500

    Added explicit engine

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `bono_module_advice_categories`;
 CREATE TABLE `bono_module_advice_categories` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Category ID',
     `name` varchar(255) NOT NULL COMMENT 'Category name'   
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 /* Advices */
 DROP TABLE IF EXISTS `bono_module_advice`;
@@ -15,7 +15,7 @@ CREATE TABLE `bono_module_advice` (
     `icon` varchar(255) NOT NULL COMMENT 'Optional icon',
 
     FOREIGN KEY (category_id) REFERENCES bono_module_advice_categories(id) DELETE SET NULL ON UPDATE CASCADE
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_advice_translations`;
 CREATE TABLE `bono_module_advice_translations` (
@@ -26,4 +26,4 @@ CREATE TABLE `bono_module_advice_translations` (
 
     FOREIGN KEY (id) REFERENCES bono_module_advice(id) ON DELETE CASCADE,
     FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
